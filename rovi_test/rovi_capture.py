e70b0819c940c74ded59d4198d5f00019e0ef985
HirokoImamiya <imamiya@yoods.co.jp>
2021-02-16 11:11:58 +0900

    rovi/Y1 Subscribe delete log delete

--- rovi_test/rovi_capture.py
+++ rovi_test/rovi_capture.py
@@ -23,7 +23,6 @@ def voxel(data):
   pc = o3d.geometry.PointCloud()
   pc.points = o3d.Vector3dVector(d)
   dwpc = o3d.geometry.voxel_down_sample(pc,voxel_size=mesh)
-  rospy.loginfo("down sample done")
   return np.reshape(np.asarray(dwpc.points),(-1, 3))
 
 def nf(data):
@@ -38,7 +37,6 @@ def nf(data):
                                                 nb_points = nfmin,
                                                 radius = nfrad)
   dwpc = o3d.geometry.select_down_sample(pc, ind)
-  rospy.loginfo("noise filter done")
   return np.reshape(np.asarray(dwpc.points),(-1, 3))
 
 def get_nfparam():
@@ -47,9 +45,6 @@ def get_nfparam():
   nfparam['mesh'] = rospy.get_param("/rovi_test/mesh", 0)
   nfparam['nfmin'] = rospy.get_param("/rovi_test/nfmin", 0)
   nfparam['nfrad'] = rospy.get_param("/rovi_test/nfrad", 0)
-
-def cb_capture(msg):
-  rospy.loginfo("[%d] capture result %s", count, msg.data)
 
 def cb_ps(msg):
   pc = np.reshape(msg.data,(-1,3))
@@ -65,20 +60,16 @@ if __name__=="__main__":
   global nfparam
   nfparam = dict()
   rospy.init_node("rovi_capture", anonymous = True)
-  rospy.Subscriber("/rovi/Y1", Bool, cb_capture)
   rospy.Subscriber("/rovi/ps_floats", numpy_msg(Floats), cb_ps)
   pub_capture = rospy.Publisher("/rovi/X1", Bool, queue_size = 1)
   pub_nf = rospy.Publisher("rovi_test/ps_floats",
                            numpy_msg(Floats),
                            queue_size = 1)
   rospy.sleep(1)
-  count = 0
   r = rospy.Rate(0.1) # 10s
   while not rospy.is_shutdown():
     if rospy.get_param("/rovi_test/pshift"):
-      count = count + 1
       msg = Bool()
       msg.data = True
-      rospy.loginfo("[%d] capture request", count)
       pub_capture.publish(msg)
     r.sleep()
